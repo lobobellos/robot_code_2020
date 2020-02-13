c816553477afbb4bb6e2fced67c0032969cc4887
Christian Stober <christian_stober@dpsk12.org>
2020-02-12 18:39:02 -0700

    Added speed controller groups & speed throttle

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,17 +12,21 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 /**
  * This is a demo program showing the use of the DifferentialDrive class.
  * Runs the motors with arcade steering.
  */
 public class Robot extends TimedRobot {
-  private final PWMVictorSPX m_leftMotor_rear = new Spark(0);
-  private final PWMVictorSPX m_leftMotor_front = new Spark(1);
-  private final PMWVictorSPX m_rightMotor_rear = new Spark(2); 
-  private final PWMVictorSPX m_rightMotor_front = new Spark(3);
-  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotor_rear, m_rightMotor_rear);
+  private final Spark m_leftMotor_rear = new Spark(0);
+  private final Spark m_leftMotor_front = new Spark(1);
+  private final SpeedControllerGroup m_left = new SpeedControllerGroup(m_leftMotor_rear, m_leftMotor_front);
+  
+  private final Spark m_rightMotor_rear = new Spark(2); 
+  private final Spark m_rightMotor_front = new Spark(3);
+  private final SpeedControllerGroup m_right = new SpeedControllerGroup(m_rightMotor_rear, m_rightMotor_front);
+  
+  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_left, m_right);
   private final Joystick m_stick = new Joystick(0);
 
   @Override
@@ -35,6 +39,9 @@ public class Robot extends TimedRobot {
     // Drive with arcade drive.
     // That means that the Y axis drives forward
     // and backward, and the X turns left and right.
-    m_robotDrive.arcadeDrive(m_stick.getY(), m_stick.getX());
+    Double speed = (-m_stick.getThrottle() + 1) / 2;
+    speed = (speed * 0.5) + 0.5;
+    System.out.println(speed);
+    m_robotDrive.arcadeDrive(m_stick.getY() * speed, m_stick.getX() * speed);
   }
 }
